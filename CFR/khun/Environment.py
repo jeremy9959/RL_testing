292331ea00a92c23b0cdab88712c91ad895c94b6
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2025-02-24 13:37:43 -0500

    some formatting changes from ruff

--- CFR/khun/Environment.py
+++ CFR/khun/Environment.py
@@ -1,20 +1,17 @@
-
 class KhunEnv:
     def __init__(self, players):
         self.N = players  # Players including chance, dictionary of id's that map onto player objects
         self.H = tuple()
-        self.sequence = ['c', 'c', 1, 2, 1]
+        self.sequence = ["c", "c", 1, 2, 1]
         self.seq_id = 0
         self.pot = 0
 
     def get_next_turn(self):
-
         return self.sequence[self.seq_id]
-
 
     def sample_action(self, i, a):
         ### Update information sets for each player
-        if i == 'c':
+        if i == "c":
             if len(self.H) == 0:
                 self.N[1].card = a
                 self.pot += 1
@@ -27,10 +24,10 @@ class KhunEnv:
                 self.N[2].I = (a,)
 
         else:
-            if a == 'B':
+            if a == "B":
                 self.N[i].stake += 1
                 self.pot += 1
-            elif a == 'C':
+            elif a == "C":
                 self.N[i].stake += 1
                 self.pot += 1
             self.N[1].I += (a,)
@@ -38,34 +35,29 @@ class KhunEnv:
         self.H += (a,)
         self.seq_id += 1
 
-
     def is_terminal(self) -> bool:
-
         if self.H is None or len(self.H) <= 3:
             return False
         elif len(self.H) == 4:
-            if self.H[3] == 'P' or self.H[3] == 'C' or self.H[3] == 'F':
+            if self.H[3] == "P" or self.H[3] == "C" or self.H[3] == "F":
                 return True
             else:
                 return False
         else:
             return True
 
-
-
     def utility(self) -> dict:
         u = dict()
         winner, looser = self.winner()
 
-        if self.H[3] == 'P' or self.H[3] == 'C':
-
+        if self.H[3] == "P" or self.H[3] == "C":
             u[winner] = self.pot - self.N[1].stake
             u[looser] = -self.N[2].stake
-        elif self.H[3] == 'F':
+        elif self.H[3] == "F":
             u[1] = self.pot - self.N[1].stake
             u[2] = -self.N[2].stake
 
-        elif self.H[4] == 'C':
+        elif self.H[4] == "C":
             u[winner] = self.pot - self.N[1].stake
             u[looser] = -self.N[2].stake
         else:
@@ -74,10 +66,15 @@ class KhunEnv:
         return u
 
     def winner(self):
-        if self.N[1].card == 'K': return 1, 2
-        elif self.N[2].card == 'K': return 2, 1
-        elif self.N[1].card == 'Q': return 1, 2
-        elif self.N[2].card == 'Q': return 2, 1
+        if self.N[1].card == "K":
+            return 1, 2
+        elif self.N[2].card == "K":
+            return 2, 1
+        elif self.N[1].card == "Q":
+            return 1, 2
+        elif self.N[2].card == "Q":
+            return 2, 1
+
     @staticmethod
     def possible_actions(I) -> dict:
         """
@@ -85,14 +82,12 @@ class KhunEnv:
         :return dictionary that maps a|I to R(I,a) set to  0 initially:
         """
         if len(I) == 1:
-            return {'P': 0, 'B': 0}
+            return {"P": 0, "B": 0}
         elif len(I) == 2:
-            if I[1] == 'B':
-                return {'F': 0, 'C': 0}
+            if I[1] == "B":
+                return {"F": 0, "C": 0}
 
-            elif I[1] == 'P':
-                return {'P': 0, 'B': 0}
+            elif I[1] == "P":
+                return {"P": 0, "B": 0}
         elif len(I) == 3:
-            return {'F': 0, 'C': 0}
-
-
+            return {"F": 0, "C": 0}
--- CFR/khun/Experiment.py
+++ CFR/khun/Experiment.py
@@ -1,27 +1,35 @@
 from copy import deepcopy
-from attr import *
-from Environment import *
+from attr import Chance, Player
+from Environment import KhunEnv
 from tqdm import tqdm
+
+
 def traverse(a, env, i, players):
     env = deepcopy(env)
     r = dict()
     g = 0
-    if env.is_terminal(): return env.utility()[i.i]
-    if a is not None: env.sample_action(i.i, a)
-    if env.is_terminal(): return env.utility()[i.i]
+    if env.is_terminal():
+        return env.utility()[i.i]
+    if a is not None:
+        env.sample_action(i.i, a)
+    if env.is_terminal():
+        return env.utility()[i.i]
 
     t = env.get_next_turn()
     while t != i.i:
 
-        if env.is_terminal(): return env.utility()[i.i]
-        if t != 'c':
+        if env.is_terminal():
+            return env.utility()[i.i]
+        if t != "c":
             a = players[t].sample(env.N[t].I)
             prob = players[t].get_action_probability(env.N[t].I, a)
             players[t].accum_pol(env.N[t].I, a, prob)
-        else: a = env.N['c'].sample()
+        else:
+            a = env.N["c"].sample()
 
         env.sample_action(t, a)
-        if env.is_terminal(): return env.utility()[i.i]
+        if env.is_terminal():
+            return env.utility()[i.i]
         t = env.get_next_turn()
 
     for a in i.c_Regret[env.N[i.i].I].keys():
@@ -35,9 +43,8 @@ def traverse(a, env, i, players):
     return g
 
 
-
-if __name__ == '__main__':
-    players = {1: Player(1), 2: Player(2), 'c': Chance()}
+if __name__ == "__main__":
+    players = {1: Player(1), 2: Player(2), "c": Chance()}
     env = KhunEnv(players)
     for t in tqdm(range(100000)):
         for j in range(1, 3, 1):
@@ -46,6 +53,3 @@ if __name__ == '__main__':
     print(players[1].count)
     print(players[2].get_average_strategy())
     print(players[2].count)
-
-
-
